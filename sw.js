9788a4d9fe7a780124f1889532ac372bd5b77130
ott3r07 <1029mins@gmail.com>
2022-05-22 14:14:50 +0900

    Updates

--- sw.js
+++ sw.js
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-58f863d6aa18c11a521f.js"
   },
   {
-    "url": "app-d3943095c143c9b9355b.js"
+    "url": "app-5545e7e14cbdd8cfe66c.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-15096bed533ed0ff8b58.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "be81423d99cad8c3ef4695493935748d"
+    "revision": "f978c315ee64d7444e80c4e50e543672"
   },
   {
     "url": "polyfill-c5f50f2ad13861331c8a.js"
@@ -76,7 +76,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-d3943095c143c9b9355b.js`))) {
+  if (!resources || !(await caches.match(`/app-5545e7e14cbdd8cfe66c.js`))) {
     return await fetch(event.request)
   }
 
